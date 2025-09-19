63f311e4849e349c04e020f2d0d568bf23943fb3
GhostDevelopments <davidmoore019@gmail.com>
2025-09-19 19:22:35 +0100

    Add files via upload

---
+++ adminplus-selldrugs/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'AdminPlus#0001 | AdminPlus'
+description 'AdminPlus Sell Drugs w/ Ox_Lib'
+lua54 'yes'
+version '2.0'
+
+shared_script '@ox_lib/init.lua'
+
+client_scripts{
+    'config.lua',
+    'client.lua',
+}
+
+server_scripts{
+    'config.lua',
+    'server.lua',
+}
+
